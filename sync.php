c8c146956790899398f0eebec33264e2488c5b92
Mayur Pipaliya <mayurpipaliya@gmail.com>
2017-07-06 08:23:13 -0400

    changes

--- sync.php
+++ sync.php
@@ -1,9 +1,11 @@
 <?php 
-
-system('hostname && uptime');
-
+echo '||';
+system('hostname');
+echo '||';
+sysmte('uptime');
+echo '||';
 system('ifconfig');
-
+echo '||';
 ?>
 <meta http-equiv="refresh" content="3"> 
 <!-- ensure that you've round robin set whenever you utilise multiple EC2 and ELB with auto scalling group. -->
