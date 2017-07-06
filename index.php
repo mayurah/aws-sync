42a9697850a6ee566790500cbd0834d2634b6e55
Mayur Pipaliya <mayurpipaliya@gmail.com>
2017-07-06 08:22:08 -0400

    source

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+echo 'HackForward :: AWS';
+
+?>
---
+++ sync.php
@@ -0,0 +1,9 @@
+<?php 
+
+system('hostname && uptime');
+
+system('ifconfig');
+
+?>
+<meta http-equiv="refresh" content="3"> 
+<!-- ensure that you've round robin set whenever you utilise multiple EC2 and ELB with auto scalling group. -->
